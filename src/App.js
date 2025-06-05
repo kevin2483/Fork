479cfb4c2d04ee2466f867292f9550bb60b83afa
안정재 <kevin2483@naver.com>
2025-06-05 09:39:24 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,131 @@
+import { useState } from "react";
+import "./styles.css";
+
+function Square({ value, onSquareClick, isWinningSquare }) {
+  return (
+    <button
+      className={isWinningSquare ? "square color" : "square"}
+      onClick={onSquareClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+function Board({ xIsNext, squares, onPlay }) {
+  const winnerLine = calculateWinner(squares);
+
+  function handleClick(i) {
+    if (winnerLine || squares[i]) return;
+    const nextSquares = squares.slice();
+    nextSquares[i] = xIsNext ? "X" : "O";
+    onPlay(nextSquares);
+  }
+
+  let status;
+  if (winnerLine) {
+    status = "Winner: " + squares[winnerLine[0]];
+  } else if (!squares.includes(null)) {
+    status = "draw!";
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+  function renderSquare(i) {
+    const isWinningSquare = winnerLine?.includes(i);
+    return (
+      <Square
+        key={i}
+        value={squares[i]}
+        onSquareClick={() => handleClick(i)}
+        isWinningSquare={isWinningSquare}
+      />
+    );
+  }
+
+  const boardRows = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+  ];
+
+  return (
+    <>
+      <div className="status">{status}</div>
+      {boardRows.map((row, rowIndex) => (
+        <div key={rowIndex} className="board-row">
+          {row.map((i) => renderSquare(i))}
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const [sortAscending, setSortAscending] = useState(true);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  function toggleSortOrder() {
+    setSortAscending(!sortAscending);
+  }
+
+  let moves = history.map((_, move) => {
+    const description = move > 0 ? "Go to move #" + move : "Go to game start";
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  if (!sortAscending) {
+    moves.reverse();
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <button onClick={toggleSortOrder}>
+          {sortAscending ? "오름차순" : "내림차순"}
+        </button>
+        <ol reversed={!sortAscending}>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return [a, b, c]; // 승리한 index들 반환
+    }
+  }
+  return null;
+}
